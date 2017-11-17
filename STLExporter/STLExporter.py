6e16c1d989d4f4916fa903926aa984a120d53253
Pops Dylan <33648856+PopsDylan@users.noreply.github.com>
2017-11-17 08:56:32 -0800

    Create STLExporter

---
+++ STLExporter/STLExporter.py
@@ -0,0 +1,86 @@
+#Author-PopsDylan
+#Description-Exports component bodies as STL.
+
+import adsk.core, adsk.fusion, adsk.cam, traceback
+import os.path
+
+
+
+def collect_bodies_to_export(design):
+    root = design.rootComponent
+    occurrences = root.allOccurrences
+    
+    bodies_to_export = []    
+    
+
+    try:
+        for comp in design.allComponents:
+            supplier, partnumber = [s.strip() for s in (comp.partNumber.split(':', 1) if ':' in comp.partNumber else ('Unknown', comp.partNumber))]
+
+            if supplier == 'Printed Part':
+                bodies = comp.bRepBodies
+            
+                if bodies.count == 1:
+                    # add to collection for export
+                    bodies_to_export.append ((bodies.item(0), partnumber))
+                else:
+                        raise Exception('Component ' + comp.name + ' is a printed part with ' + str(bodies.count) + ' bodies.')
+    
+    except:
+        app = adsk.core.Application.get()
+        ui  = app.userInterface   
+        if ui:
+            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
+            
+        bodies_to_export = []
+        
+    return bodies_to_export
+    
+def export_stls(design):
+    bodies_to_export = collect_bodies_to_export(design)
+        
+    app = adsk.core.Application.get()
+    ui  = app.userInterface    
+
+    if bodies_to_export:
+        folderDlg = ui.createFolderDialog();
+        folderDlg.title = 'STL Export Folder'
+
+        dlgResult = folderDlg.showDialog()
+    
+        if dlgResult == adsk.core.DialogResults.DialogOK:
+            save_folder = folderDlg.folder
+            
+            exportManager = design.exportManager
+        
+            for body, filename in bodies_to_export:
+                filepath = os.path.join(save_folder, filename) 
+                stlExportOptions = exportManager.createSTLExportOptions(body, filepath)
+                stlExportOptions.sendToPrintUtility = False
+                
+                exportManager.execute(stlExportOptions)
+                
+        if ui:
+            ui.messageBox('Exported ' + str(len(bodies_to_export)) + ' STL files to ' + save_folder)
+    else:
+        if ui:
+            ui.messageBox('Exported no files.')
+    
+def run(context):
+    ui = None
+    try:
+        app = adsk.core.Application.get()
+        ui  = app.userInterface
+     
+        product = app.activeProduct
+        design = adsk.fusion.Design.cast(product)
+        title = 'STLExporter'
+        if not design:
+            ui.messageBox('No active design', title)
+            return
+            
+        export_stls(design)
+
+    except:
+        if ui:
+            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
