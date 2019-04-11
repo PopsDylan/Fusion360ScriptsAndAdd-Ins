21250ebeef49aa7f26169069e8d98b472fcaffd6
PopsDylan <33648856+PopsDylan@users.noreply.github.com>
2019-04-10 19:43:57 -0700

    Filenames indicate the number of copies to print.

--- STLExporter/STLExporter.py
+++ STLExporter/STLExporter.py
@@ -18,12 +18,19 @@ def collect_bodies_to_export(design):
             supplier, partnumber = [s.strip() for s in (comp.partNumber.split(':', 1) if ':' in comp.partNumber else ('Unknown', comp.partNumber))]
 
             if supplier == 'Printed Part':
-                bodies = comp.bRepBodies
+                occurrence_count = root.allOccurrencesByComponent(comp).count
+
+                if occurrence_count != 0:
+                    bodies = comp.bRepBodies
             
-                if bodies.count == 1:
-                    # add to collection for export
-                    bodies_to_export.append ((bodies.item(0), partnumber))
-                else:
+                    if bodies.count == 1:
+                        # add to collection for export
+                        filename = str(partnumber)
+                        
+                        if occurrence_count > 1:
+                            filename += "_x" + str(occurrence_count)
+                        bodies_to_export.append ((bodies.item(0), filename))
+                    else:
                         raise Exception('Component ' + comp.name + ' is a printed part with ' + str(bodies.count) + ' bodies.')
     
     except:
@@ -57,14 +64,15 @@ def export_stls(design):
                 filepath = os.path.join(save_folder, filename) 
                 stlExportOptions = exportManager.createSTLExportOptions(body, filepath)
                 stlExportOptions.sendToPrintUtility = False
+                stlExportOptions.isBinaryFormat = False
                 
                 exportManager.execute(stlExportOptions)
                 
-        if ui:
-            ui.messageBox('Exported ' + str(len(bodies_to_export)) + ' STL files to ' + save_folder)
+            if ui:
+                ui.messageBox('Exported ' + str(len(bodies_to_export)) + ' STL files to ' + save_folder)
     else:
         if ui:
-            ui.messageBox('Exported no files.')
+            ui.messageBox('Found no printed parts to export.')
     
 def run(context):
     ui = None
